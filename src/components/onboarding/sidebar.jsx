e4bc0354c4e8a7526110eda363e8a79daf7c928a
mohubu <109273679+mohubu@users.noreply.github.com>
2024-04-13 18:06:43 +0000

    Dashboard cleanup

--- src/components/onboarding/sidebar.jsx
+++ src/components/onboarding/sidebar.jsx
@@ -20,7 +20,7 @@ const drawerWidth = 300
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
-  padding: theme.spacing(0, 1),
+  padding: theme.spacing(0, 0),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
   justifyContent: 'flex-end',
