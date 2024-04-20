7f798015cb5d0c8d35dfbd9b7b2c07a191d6373e
mohubu <109273679+mohubu@users.noreply.github.com>
2024-04-20 13:05:09 +0000

    Sign up screen modification

--- src/components/onboarding/Signup.jsx
+++ src/components/onboarding/Signup.jsx
@@ -1,50 +1,109 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import CardContent from "@mui/material/CardContent";
+import Card from "@mui/material/Card";
 import "./Onboarding.css";
 import logo from "../../assets/logo.svg";
-import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import PersonIcon from "@mui/icons-material/Person";
+import BuildIcon from "@mui/icons-material/Build";
+import BusinessIcon from "@mui/icons-material/Business";
+
 const baseURL = "https://orionbackend-1.onrender.com";
 
 const Signup = () => {
   const googlelogin = `${baseURL}/login`;
 
   return (
-    <Stack sx={{ maxWidth: 878 }} variant="outlined" className="card">
-      <CardContent className="Welcome">
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+      className="signup-container"
+      style={{ minHeight: "100vh" }}
+    >
+      <Grid item>
         <img src={logo} alt="logo" />
-      </CardContent>
+      </Grid>
 
-      <CardContent className="Welcome">
-        <h3>Welcome to Resiix</h3>
-      </CardContent>
+      <Grid item>
+        <CardContent className="Welcome">
+          <h3>Welcome to Resiix</h3>
+        </CardContent>
+      </Grid>
 
-      <CardContent>
-        <a href="/resiix">
-          <Button className="signup_button" variant="contained">
-            proceed as a Tenant
-          </Button>
-        </a>
-      </CardContent>
+      <Grid item container justifyContent="center">
+        <div className="cards-container">
+          <Grid item xs={12} sm={4}>
+            <div className="card-wrapper">
+              <Card className="card">
+                <CardContent className="card-content">
+                  <PersonIcon fontSize="large" />
+                  <h4>Tenant</h4>
+                </CardContent>
+              </Card>
+              <div className="button-wrapper">
+                <Button
+                  className="signup_button"
+                  variant="contained"
+                  component={Link}
+                  to="/resiix"
+                >
+                  Proceed 
+                </Button>
+              </div>
+            </div>
+          </Grid>
 
-      <CardContent>
-        <a href="/resiix">
-          <Button className="signup_button" variant="contained">
-            proceed as a Technician
-          </Button>
-        </a>
-      </CardContent>
+          <Grid item xs={12} sm={4}>
+            <div className="card-wrapper">
+              <Card className="card">
+                <CardContent className="card-content">
+                  <BuildIcon fontSize="large" />
+                  <h4>Technician</h4>
+                </CardContent>
+              </Card>
+              <div className="button-wrapper">
+                <Button
+                  className="signup_button"
+                  variant="contained"
+                  component={Link}
+                  to="/resiix"
+                >
+                  Proceed 
+                </Button>
+              </div>
+            </div>
+          </Grid>
 
-      <CardContent>
-        <Link className="green" to={googlelogin}>
-          <Button className="signup_button" variant="contained">
-            Login with Google
-          </Button>
-        </Link>
-      </CardContent>
-    </Stack>
+          <Grid item xs={12} sm={4}>
+            <div className="card-wrapper">
+              <Card className="card">
+                <CardContent className="card-content">
+                  <BusinessIcon fontSize="large" />
+                  <h4>Property Manager</h4>
+                </CardContent>
+              </Card>
+              <div className="button-wrapper">
+                <Link className="green" to={googlelogin}>
+                  <Button
+                    className="signup_button"
+                    variant="contained"
+                    component={Link}
+                    to={googlelogin}
+                  >
+                    Login with Google
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </Grid>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
